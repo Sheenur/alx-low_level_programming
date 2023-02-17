287c9b83f5a72057ebbd8d01da693f79cb15edca
sheenur <sheenur.dare@gmail.com>
2023-02-16 20:51:52 -0800

    Long integer float

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main . A program that prints the variou computer size
+ * return; 0
+ */
+int main(void)
+{
+  char a;
+  int b;
+  long int c;
+  long long d;
+  float e;
+
+printf("size of a char;" %lu "byte(s)\n"(unassigned long)sizeof(a))
+printf("size of init;" %lu "byte(s)\n"(unassigned long)sizeof(b))
+printf("size of a long intr;" %lu "byte(s)\n"(unassigned long)sizeof(c))
+  printf("size of a char;" %lu "byte(s)\n"(unassigned long)sizeof(d))
+  }
