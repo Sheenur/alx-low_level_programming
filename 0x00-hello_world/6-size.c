9e61c8f25ce1a2ab20b8c2d5ce5ee5f6b0439471
sheenur <sheenur.dare@gmail.com>
2023-02-16 22:25:25 -0800

    Float integer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,9 +10,9 @@ int main(void)
   long int c;
   long long d;
   float e;
-
-printf("size of a char;" %lu "byte(s)\n"(unassigned long)sizeof(a))
-printf("size of init;" %lu "byte(s)\n"(unassigned long)sizeof(b))
-printf("size of a long intr;" %lu "byte(s)\n"(unassigned long)sizeof(c))
-  printf("size of a char;" %lu "byte(s)\n"(unassigned long)sizeof(d))
-  }
+  printf("size of a char: %lu "byte(s)\n", (unassigned long)sizeof(a));
+  printf("size of iit: %lu "byte(s)"\n", (unassigned long)sizeof(b));
+  printf("size of a long int: %lu "byte(s)"\n", (unassigned long)sizeof(c));
+  printf("size of a float: %lu "byte(s)"\n", (unassigned long)sizeof(e));
+  printf("size of a long long int: %lu "byte(s)"\n",(unassigned long)sizeof(d));
+}
