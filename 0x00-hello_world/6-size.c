4136d6aa9f58e5fa53fd4e40247e24107cb4b452
sheenur <sheenur.dare@gmail.com>
2023-02-16 22:51:55 -0800

    Float chsr

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,11 @@
-#include<stdio.h>
-/**
- * main . A program that prints the variou computer size
- * return; 0
- */
+#include <stdio.h>
+
 int main(void)
 {
-  char a;
-  int b;
-  long int c;
-  long long d;
-  float e;
-  printf("size of a char: %lu "byte(s)\n", (unassigned long)sizeof(a));
-  printf("size of iit: %lu "byte(s)"\n", (unassigned long)sizeof(b));
-  printf("size of a long int: %lu "byte(s)"\n", (unassigned long)sizeof(c));
-  printf("size of a float: %lu "byte(s)"\n", (unassigned long)sizeof(e));
-  printf("size of a long long int: %lu "byte(s)"\n",(unassigned long)sizeof(d));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+  return 0;
 }
