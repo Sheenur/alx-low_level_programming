ab666d8159cfc2fdf60350b0178fe26afd42f636
sheenur <sheenur.dare@gmail.com>
2023-02-20 21:32:12 -0800

    Alphabet soup missing eq

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main() {
+  char letter = 'a';
+
+  while (letter <= 'z') {
+    if (letter != 'e' && letter != 'q') {
+      putchar(letter);
+    }
+    letter++;
+  }
+
+  putchar('\n');
+  return 0;
+}
