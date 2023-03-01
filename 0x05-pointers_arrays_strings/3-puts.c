d3bacb61be357d0574c361aef3c5698c0b1c4a3c
sheenur <sheenur.dare@gmail.com>
2023-03-01 03:54:06 -0800

    Reverse gear

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * _puts -print a string, followed by a new line to stdout
+ * _puts - prints a string, followed by a new line to stdout
  * @str: string to print
  */
 void _puts(char *str)
@@ -8,6 +9,6 @@ void _puts(char *str)
 while (*str != '\0')
 {
 _putchar(*str++);
-_putchar('\n');
 }
+_putchar('\n');
 }
