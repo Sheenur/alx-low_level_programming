cd942d6ac08a3a59aa814ed9fd6fd36291c90702
sheenur <sheenur.dare@gmail.com>
2023-03-02 06:20:17 -0800

    Lowercase string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: pointer to the string to convert
+ *
+ * Return: pointer to the converted string
+ */
+char *string_toupper(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
+}
+
+return str;
+}
