227be81c93f60c751dd372441d21021267f8e1a6
sheenur <sheenur.dare@gmail.com>
2023-03-02 07:11:00 -0800

    Rot in

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: the string to encode
+ *
+ * Return: a pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+  int i;
+
+  for (i = 0; str[i] != '\0'; i++)
+    {
+      if ((str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm'))
+        {
+	  str[i] += 13;
+        }
+      else if ((str[i] >= 'N' && str[i] <= 'Z') || (str[i] >= 'n' && str[i] <= 'z'))
+        {
+	  str[i] -= 13;
+        }
+    }
+
+  return str;
+}
