e7b225c3efd90811e42f9b064a2cd4db4ef70761
sheenur <sheenur.dare@gmail.com>
2023-03-06 13:38:10 -0800

    Dufficalt game is chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard - Prints the chessboard
@@ -13,7 +13,7 @@ int i, j;
 for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
-_putchar(a[i][j]);
-_putchar('\n');
+putchar(a[i][j]);
+putchar('\n');
 }
 }
--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * _putchar - Writes a character to the standard output (stdout)
  * @c: The character to be printed
