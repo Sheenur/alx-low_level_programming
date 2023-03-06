960ad79d4257adccd490f9c664ae56d970d12cc5
sheenur <sheenur.dare@gmail.com>
2023-03-06 07:06:34 -0800

    String pbrk

---
+++ 0x07-pointers_arrays_strings/4-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strpbrk(char *s, char *accept);
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strpbrk(s, f);
+    printf("%s\n", t);
+    return (0);
+}
