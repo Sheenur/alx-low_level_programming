13805032c20afc8e803f30c324434fe6ce6fdc10
sheenur <sheenur.dare@gmail.com>
2023-03-07 14:03:13 -0800

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int _strlen_recursion(char *s);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  int n;
+
+  n = _strlen_recursion("Corbin Coleman");
+  printf("%d\n", n);
+  return (0);
+}
