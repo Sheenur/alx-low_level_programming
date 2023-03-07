6cd3fee769aef28d7e77e36e3dc35ff943de9e34
sheenur <sheenur.dare@gmail.com>
2023-03-07 00:49:14 -0800

    Set string

--- 0x07-pointers_arrays_strings/100-main.c
+++ 0x07-pointers_arrays_strings/100-main.c
@@ -9,11 +9,11 @@ void set_string(char **s, char *to);
  */
 int main(void)
 {
-	char *s0 = "Bob Dylan";
-	char *s1 = "Robert Allen";
+    char *s0 = "Bob Dylan";
+    char *s1 = "Robert Allen";
 
-	printf("%s, %s\n", s0, s1);
-	set_string(&s1, s0);
-	printf("%s, %s\n", s0, s1);
-	return (0);
+    printf("%s, %s\n", s0, s1);
+    set_string(&s1, s0);
+    printf("%s, %s\n", s0, s1);
+    return (0);
 }
