3db7329d040dfeba4917378ca1b8f0697ea3fea7
sheenur <sheenur.dare@gmail>
2023-06-01 23:23:16 -0700

    Agrred that it is OK

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -3,12 +3,12 @@
 /**
  * infinite_add - C function that adds two numbers stored
  *in strings to a buffer.
- *Assumes that strings are never empty and
+ * Assumes that strings are never empty and
  *that numbers will always be positive, or 0.
- *Assumes there are only digits stored in the number strings.
- *If result can be stored in the buffer,
+ * Assumes there are only digits stored in the number strings.
+ * If result can be stored in the buffer,
  *returns a pointer to the result.
- *If result cannot be stored in the buffer, returns `0`.
+ * If result cannot be stored in the buffer, returns `0`.
  *@n1:first number to be added
  *@n2:second number to be added
  *@r: store result
@@ -18,6 +18,9 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+  /* Your code here */
+     return NULL; // Replace with the appropriate return statement
+
 }
 
 /**
@@ -43,18 +46,19 @@ num += tens;
 tens = num / 10;
 }
 
-for (; *n1; n1--; r_index++)
+for (; *n1; n1--, r_index++)
 {
 num = *(n1 - '0') + tens; 
 *(r + r_index) = (num % 10) + '0';
 tens = num / 10;
 }
 
-for (; *n2; n2--;  r_index--)
+for (; *n2; n2--,  r_index--)
 {
 num = (*n2 - '0') + tens; 
 *(r + r_index) = (num % 10) + '0';
 tens = num / 10; 
 }
-  
+return r; // Add the appropriate return statement
 }
+
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,36 +17,34 @@
 void print_buffer(char *b, int size)
 {
 int i = 0, j;
-
-if (size < 0)
+if (size <= 0)
 {
-printf('\n');
+printf("\n");
 return;
 }
 
-while (i < size)
+for (i = 0; i < size; i += 10)
 {
-if (i % 10 == 0)
 printf("%08x: ", i);
-for (j = i; j < i + 9; j += 2)
+for (j = i; j < i + 10; j++)
 {
-if ((j < size) && ((j + 1) < size))
-printf("%02x%02x: ", b[j], b[j + 1]);
+if (j < size)
+printf("%02x", b[j]);
 else
+printf("  ");
+if (j % 2 != 0)
+printf(" ");
+}
+for (j = i; j < i + 10; j++)
 {
-while (++j <= i + 10)
-printf(" ");
-printf(" ");
-}
-}
-for (j = i; j < i + 9 && j < size; j++)
+if (j < size)
 {
 if (b[j] >= 32 && b[j] <= 126)
 printf("%c", b[j]);
 else
 printf(".");
 }
-printf('\n');
-i += 10;
+}
+printf("\n");
 }
 }
